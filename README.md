fde3f82b86c66da71d10762939689169d680d968
Michael Lutsiuk <michael.lutsiuk@gmail.com>
2017-01-27 22:46:23 +0200

    Leave (hopeful) useful comments

--- README.md
+++ README.md
@@ -1,7 +1,19 @@
 # ress
 A Fast RSS Aggregator
 
-# Motivation
+## Motivation
 I'm working on this rss aggregator as an alternative for tiny tiny rss, 
 it should have more consistent, minimalistic api and should be faster
 when running on my raspberry pi.
+
+## API
+
+```
+GET 		/feeds
+POST 		/feeds
+GET 		/feeds/[0-9]+
+DELETE 	/feeds/[0-9]+
+GET 		/feeds/[0-9]+/items
+POST 		/feeds/[0-9]+/items/read
+DEETE 	/feeds/[0-9]+/items/read
+```
--- db/sqlite/migrations.go
+++ db/sqlite/migrations.go
@@ -4,6 +4,7 @@ import (
 	"github.com/rubenv/sql-migrate"
 )
 
+// Migrations holds sqlite migrations required for github.com/mluts/ress
 var Migrations = &migrate.MemoryMigrationSource{
 	Migrations: []*migrate.Migration{
 		&migrate.Migration{
@@ -21,6 +22,7 @@ var Migrations = &migrate.MemoryMigrationSource{
 					updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 				);
 
+			-- feed -> items, should be deleted if feed was deleted
 			CREATE TABLE IF NOT EXISTS items
 				(
 					id INTEGER PRIMARY KEY,
@@ -36,25 +38,31 @@ var Migrations = &migrate.MemoryMigrationSource{
 					updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
 				);
 
+			-- Holds items read/uread marks
 			CREATE TABLE IF NOT EXISTS item_reads
 				(
 					id INTEGER PRIMARY KEY,
 					item_id INTEGER REFERENCES items(id) ON DELETE CASCADE
 				);
 
+			-- Abstraction on the top of "items"
 			CREATE VIEW IF NOT EXISTS items_view AS
 				SELECT items.*, NOT ifnull(item_reads.id, 0) AS unread FROM items
 					LEFT JOIN item_reads ON item_reads.item_id = items.id;
 
+			-- items.link should not be duplicated within same feed
 			CREATE UNIQUE INDEX IF NOT EXISTS
 				feed_item_link ON items ( feed_id, link );
 
+			-- feeds.link should not be duplicated
 			CREATE UNIQUE INDEX IF NOT EXISTS
 				feed_link ON feeds(link);
 
+			-- item read/unread should not be duplicated
 			CREATE UNIQUE INDEX IF NOT EXISTS
 				item_read ON item_reads(id, item_id);
 
+			-- feeds.updated_at trigger
 			CREATE TRIGGER IF NOT EXISTS
 				feeds_updated_at AFTER UPDATE ON feeds
 				BEGIN
@@ -62,6 +70,7 @@ var Migrations = &migrate.MemoryMigrationSource{
 						WHERE id = NEW.id;
 				END;
 
+			-- items.updated_at trigger
 			CREATE TRIGGER IF NOT EXISTS
 				items_updated_at AFTER UPDATE ON items
 				BEGIN

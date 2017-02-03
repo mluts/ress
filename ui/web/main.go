069f6fe21eee705037bb6c831c8fce8d80b41743
Michael Lutsiuk <michael.lutsiuk@gmail.com>
2017-02-03 22:20:28 +0200

    Move to react

--- ui/web/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/gopherjs/gopherjs/js"
-	"github.com/gopherjs/jquery"
-)
-
-func main() {
-}
--- ui/web/webpack.config.js
+++ ui/web/webpack.config.js
@@ -5,5 +5,16 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'static/assets/js'),
     filename: 'bundle.js'
+  },
+
+  module:  {
+    loaders: {
+      test: /\.js/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015', 'react']
+      }
+    }
   }
 };

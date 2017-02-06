cfb3a4f9f4444df2c4143c8f1f9152f291268ef2
Michael Lutsiuk <michael.lutsiuk@gmail.com>
2017-02-07 00:29:25 +0200

    Reindent ContextMenu

--- ui/web/spec/components/ContextMenuSpec.js
+++ ui/web/spec/components/ContextMenuSpec.js
@@ -3,48 +3,57 @@ import ReactDOM from 'react-dom';
 import ContextMenu from "../../src/js/components/ContextMenu";
 
 describe('ContextMenu', function() {
-	var callback, root;
+  var callback, root;
 
-	const options = {
-		a: "Option A",
-		b: "Option B",
-		c: "Option C"
-	};
+  const options = {
+    a: "Option A",
+    b: "Option B",
+    c: "Option C"
+  };
 
-	const top = 100, left = 100;
+  const top = 100, left = 100;
 
-	beforeEach(function() {
-		root = document.createElement('div');
-		document.querySelector('body').appendChild(root);
+  function render() {
+    ReactDOM.render(
+      // jshint ignore:start
+      <ContextMenu top={top} left={left} options={options}
+      onSelectOption={callback} show={true} />,
+      // jshint ignore:end
+      root
+    );
+  }
 
-		callback = jasmine.createSpy("callback");
+  beforeEach(function() {
+    root = document.createElement('div');
+    document.querySelector('body').appendChild(root);
+    callback = jasmine.createSpy("callback");
 
-		ReactDOM.render(
-			// jshint ignore:start
-			<ContextMenu top={top} left={left} options={options}
-					onSelectOption={callback} show={true} />,
-			// jshint ignore:end
-			root
-		);
-	});
+    render();
+  });
 
-	it("renders div with options", function() {
-		var list = root.querySelectorAll('div > ul > li');
-		expect(list.length).toEqual(3);
-		expect(list[0].innerText).toBe(options.a);
-		expect(list[1].innerText).toBe(options.b);
-		expect(list[2].innerText).toBe(options.c);
-	});
+  it("renders div with options", function() {
+    var list = root.querySelectorAll('div > ul > li');
+    expect(list.length).toEqual(3);
+    expect(list[0].innerText).toBe(options.a);
+    expect(list[1].innerText).toBe(options.b);
+    expect(list[2].innerText).toBe(options.c);
+  });
 
-	it("calls the callback", function() {
-		root.querySelector('li').click();
-		expect(callback).toHaveBeenCalledWith(
-			Object.keys(options)[0]
-		);
-	});
+  it("calls the callback", function() {
+    root.querySelector('li').click();
+    expect(callback).toHaveBeenCalledWith(
+      Object.keys(options)[0]
+    );
+  });
 
-	it('hides after first click', function() {
-		root.querySelector('li').click();
-		expect(root.querySelectorAll('li').length).toEqual(0);
-	});
+  it('hides after first click', function() {
+    root.querySelector('li').click();
+    expect(root.querySelectorAll('li').length).toEqual(0);
+  });
+
+  it('works if was hidden once', function() {
+    root.querySelector('li').click();
+    render();
+    expect(root.querySelectorAll('li').length).toBe(3);
+  });
 });
--- ui/web/src/js/components/ContextMenu.js
+++ ui/web/src/js/components/ContextMenu.js
@@ -1,88 +1,88 @@
 import React from "react";
 
 class Option extends React.Component {
-	constructor(props) {
-		super(props);
-		this.selectOption = this.selectOption.bind(this);
-	}
+  constructor(props) {
+    super(props);
+    this.selectOption = this.selectOption.bind(this);
+  }
 
-	selectOption() {
-		this.props.callback(this.props.optionKey);
-	}
+  selectOption() {
+    this.props.callback(this.props.optionKey);
+  }
 
-	render() {
-		return (
-			// jshint ignore:start
-			<li onClick={this.selectOption}>
-				{this.props.title}
-			</li>
-			// jshint ignore:end
-		);
-	}
+  render() {
+    return (
+      // jshint ignore:start
+      <li onClick={this.selectOption}>
+        {this.props.title}
+      </li>
+      // jshint ignore:end
+    );
+  }
 }
 
 Option.propTypes = {
-	optionKey: React.PropTypes.string,
-	title: React.PropTypes.string,
-	callback: React.PropTypes.func,
+  optionKey: React.PropTypes.string,
+  title: React.PropTypes.string,
+  callback: React.PropTypes.func,
 };
 
 class ContextMenu extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = { show: props.show };
-		this.selectOption = this.selectOption.bind(this);
-	}
+  constructor(props) {
+    super(props);
+    this.state = { show: props.show };
+    this.selectOption = this.selectOption.bind(this);
+  }
 
-	style() {
-		return {
-			position: "fixed",
-			top: this.props.top,
-			left: this.props.left
-		};
-	}
+  style() {
+    return {
+      position: "fixed",
+      top: this.props.top,
+      left: this.props.left
+    };
+  }
 
-	selectOption(data) {
-		this.props.onSelectOption(data);
-		this.setState({show: false});
-	}
+  selectOption(data) {
+    this.props.onSelectOption(data);
+    this.setState({show: false});
+  }
 
-	componentWillReceiveProps(props) {
-		this.setState({show: props.show});
-	}
+  componentWillReceiveProps(props) {
+    this.setState({show: props.show});
+  }
 
-	render() {
-		if(!this.state.show) {
-			return null;
-		}
+  render() {
+    if(!this.state.show) {
+      return null;
+    }
 
-		var options = [];
+    var options = [];
 
-		for(let key in this.props.options) {
-			options.push(
-				// jshint ignore:start
-				<Option key={key} optionKey={key} title={this.props.options[key]}
-								callback={this.selectOption} />
-				// jshint ignore:end
-			);
-		}
+    for(let key in this.props.options) {
+      options.push(
+        // jshint ignore:start
+        <Option key={key} optionKey={key} title={this.props.options[key]}
+        callback={this.selectOption} />
+        // jshint ignore:end
+      );
+    }
 
-		return (
-			// jshint ignore:start
-			<div style={this.style()}>
-				<ul>{options}</ul>
-			</div>
-			// jshint ignore:end
-		);
-	}
+    return (
+      // jshint ignore:start
+      <div style={this.style()}>
+      <ul>{options}</ul>
+      </div>
+      // jshint ignore:end
+    );
+  }
 }
 
 ContextMenu.propTypes = {
-	top: React.PropTypes.number,
-	left: React.PropTypes.number,
-	options: React.PropTypes.object,
-	onSelectOption: React.PropTypes.func,
-	show: React.PropTypes.bool
+  top: React.PropTypes.number.isRequired,
+  left: React.PropTypes.number.isRequired,
+  options: React.PropTypes.object.isRequired,
+  onSelectOption: React.PropTypes.func.isRequired,
+  show: React.PropTypes.bool
 };
 
 module.exports = ContextMenu;

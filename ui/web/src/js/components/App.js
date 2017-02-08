207d708cba60a035fd7a5999e9fe750bf92db4cd
Michael Lutsiuk <michael.lutsiuk@gmail.com>
2017-02-09 00:29:09 +0200

    Fix js indentation

--- ui/web/src/js/components/App.js
+++ ui/web/src/js/components/App.js
@@ -5,100 +5,99 @@ import ContextMenu from "./ContextMenu";
 import Subscribe from "./Subscribe";
 
 class App extends React.Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.selectFeed = (data) => {
-			this.props.onSelectFeed(data);
-		};
+    this.selectFeed = (data) => {
+      this.props.onSelectFeed(data);
+    };
 
-		this.mouseX = this.mouseY = 0;
+    this.mouseX = this.mouseY = 0;
 
-		this.saveMouseCoordinates = this.saveMouseCoordinates.bind(this);
+    this.saveMouseCoordinates = this.saveMouseCoordinates.bind(this);
 
-		this.state = {
-			showContextMenu: false,
-			contextMenuOptions: {},
-			contextMenuCallback: function() {}
-		};
-	}
+    this.state = {
+      showContextMenu: false,
+      contextMenuOptions: {},
+      contextMenuCallback: function() {}
+    };
+  }
 
-	saveMouseCoordinates(e) {
-		this.mouseY = e.clientY;
-		this.mouseX = e.clientX;
-	}
+  saveMouseCoordinates(e) {
+    this.mouseY = e.clientY;
+    this.mouseX = e.clientX;
+  }
 
-	showFeedContextMenu(data) {
-		this.setState({
-			showContextMenu: true,
-			contextMenuOptions: {
-				edit: "Edit Feed"
-			},
-			contextMenuCallback: (key) => {
-				this.hideContextMenu();
-			}
-		});
-	}
+  showFeedContextMenu(data) {
+    this.setState({
+      showContextMenu: true,
+      contextMenuOptions: {
+        edit: "Edit Feed"
+      },
+      contextMenuCallback: (key) => {
+        this.hideContextMenu();
+      }
+    });
+  }
 
-	hideContextMenu() {
-		this.setState({
-			showContextMenu: false,
-			contextMenuOptions: {},
-			contextMenuCallback: function() {}
-		});
-	}
+  hideContextMenu() {
+    this.setState({
+      showContextMenu: false,
+      contextMenuOptions: {},
+      contextMenuCallback: function() {}
+    });
+  }
 
-	componentWillMount() {
-		window.document.addEventListener('mousemove', this.saveMouseCoordinates);
-	}
+  componentWillMount() {
+    window.document.addEventListener('mousemove', this.saveMouseCoordinates);
+  }
 
-	componentWillUnmount() {
-		window.document.removeEventListener('mousemove', this.saveMouseCoordinates);
-	}
+  componentWillUnmount() {
+    window.document.removeEventListener('mousemove', this.saveMouseCoordinates);
+  }
 
-	render() {
-		let items = [];
+  render() {
+    let items = [];
 
-		for(let feed of this.props.feeds) {
-			if(feed.Selected) {
-				items = feed.Items;
-				console.log(items);
-				break;
-			}
-		}
+    for(let feed of this.props.feeds) {
+      if(feed.Selected) {
+        items = feed.Items;
+        break;
+      }
+    }
 
-		return (
-			// jshint ignore:start
-			<div className="row">
-			<div className="col-md-3">
-			<Feeds data={this.props.feeds} onSelectFeed={this.selectFeed}
-			onFeedContextMenu={this.showFeedContextMenu} />
-			</div>
+    return (
+      // jshint ignore:start
+      <div className="row">
+      <div className="col-md-3">
+      <Feeds data={this.props.feeds} onSelectFeed={this.selectFeed}
+      onFeedContextMenu={this.showFeedContextMenu} />
+      </div>
 
-			<div className="col-md-9">
-			<div className="row">
-			<Subscribe onSubmit={this.props.onSubscribeToFeed} />
-			</div>
-			<div className="row">
-			<Items data={items} onSelectItem={this.props.onSelectItem} />
-			</div>
-			</div>
+      <div className="col-md-9">
+      <div className="row">
+      <Subscribe onSubmit={this.props.onSubscribeToFeed} />
+      </div>
+      <div className="row">
+      <Items data={items} onSelectItem={this.props.onSelectItem} />
+      </div>
+      </div>
 
-			<ContextMenu top={this.mouseY} left={this.mouseX}
-			show={this.state.showContextMenu} options={this.state.contextMenuOptions}
-			onSelectOption={this.state.contextMenuCallback}/>
+      <ContextMenu top={this.mouseY} left={this.mouseX}
+      show={this.state.showContextMenu} options={this.state.contextMenuOptions}
+      onSelectOption={this.state.contextMenuCallback}/>
 
-			</div>
-			// jshint ignore:end
-		);
-	}
+      </div>
+      // jshint ignore:end
+    );
+  }
 }
 
 App.propTypes = {
-	feeds: React.PropTypes.array.isRequired,
-	onSelectFeed: React.PropTypes.func.isRequired,
-	onSubscribeToFeed: React.PropTypes.func.isRequired,
-	onSelectItem: React.PropTypes.func.isRequired
+  feeds: React.PropTypes.array.isRequired,
+  onSelectFeed: React.PropTypes.func.isRequired,
+  onSubscribeToFeed: React.PropTypes.func.isRequired,
+  onSelectItem: React.PropTypes.func.isRequired
 };
 
 module.exports = App;
